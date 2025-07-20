fd84e6fbf17dbaeac5ae16d568d4c3a50144b835
sjunior863@gmail.com <sjunior863@gmail.com>
2025-07-20 19:38:06 -0300

    kittie generator second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import GetCats from "./components/GetCats";
 
 function App() {
   return (
-    <div>
-      <img
-        src="https://64.media.tumblr.com/tumblr_lw2190wOi41r5qrimo1_400.gif"
-        alt="Carregando..."
-        className="w-60  h-55 rounded-full
-        "
-      />
-      <div>
+    <>
+      <header
+        className="fixed top-0 left-0 w-full shadow-md z-50 p-4"
+        style={{ backgroundColor: "#242424" }}
+      >
+        <h1 className="text-3xl font-bold text-center">Cats Gallery 🐱</h1>
+      </header>
+
+      <main className="pt-20 p-4">
+        <img
+          src="https://64.media.tumblr.com/tumblr_lw2190wOi41r5qrimo1_400.gif"
+          alt="Carregando..."
+          className="w-60 h-55 rounded-full mx-auto mb-6"
+        />
         <GetCats />
-      </div>
-    </div>
+      </main>
+    </>
   );
 }
 
--- src/components/GetCats.jsx
+++ src/components/GetCats.jsx
@@ -19,23 +19,35 @@ function GetCats() {
 
   return (
     <div className="flex items-center w-full h-50">
-      <div className="flex flex-wrap gap-4 justify-center mt-4 px-7 py-10">
-        {gatinhos?.map((gato, idx) => (
-          <img
-            key={idx}
-            src={gato.url}
-            alt="gatinho"
-            className="w-32 h-32 rounded-md"
-          />
-        ))}
-      </div>
-
       {!mostrouGatos ? (
         <ButtonGetCat onClick={handleBuscarGatin} label="Gerar Gatinhos" />
       ) : (
         <button onClick={limpar}>
           <Delete />
         </button>
+      )}
+
+      {mostrouGatos && (
+        <div
+          onClick={limpar}
+          style={{ backgroundColor: "#242424" }}
+          className="fixed top-20 left-0 right-0 bottom-0 bg-black bg-opacity-70 flex justify-center items-start z-50 p-4 overflow-auto"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            style={{ backgroundColor: "#242424" }}
+            className=" p-6 rounded-md max-w-4xl max-h-[80vh] overflow-auto flex flex-wrap gap-4 justify-center border border-violet-950 border-l-rose-900"
+          >
+            {gatinhos?.map((gato, idx) => (
+              <img
+                key={idx}
+                src={gato.url}
+                alt="gatinho"
+                className="w-32 h-32 rounded-md"
+              />
+            ))}
+          </div>
+        </div>
       )}
 
       {loading && (
